0f1c70aaf8be03b292b8a4faa9262cbd704438ce
Leticia <vasquezesperanza239@gmail.com>
2026-09-18 17:03:05 -0600

    Cambios del parcial

---
+++ Parcial_VasquezPacheco.py
@@ -0,0 +1,9 @@
+print("Bienvenido al programa")
+
+nombre=input("Ingrese su nombre:")
+Animal=input("Ingrese el nombre de mascota favorita: ")
+Fecha=input(float("Ingrese la fecha por favor"))
+
+print(nombre, "Que lindo nombre")
+print ("Tu animal favorito es:", Animal)
+print(Fecha)
