b6a61d0422f18514161570879c8cda07324e96cd
Leticia <vasquezesperanza239@gmail.com>
2026-09-18 17:12:17 -0600

    Hola mundo

--- Parcial_VasquezPacheco.py
+++ Parcial_VasquezPacheco.py
@@ -7,3 +7,15 @@ Fecha=input(float("Ingrese la fecha por favor"))
 print(nombre, "Que lindo nombre")
 print ("Tu animal favorito es:", Animal)
 print(Fecha)
+
+print("Suma de numeros")
+
+n1 = int(input("Ingrese un numero: "))
+n2 = int(input("Ingrese un numero: "))
+
+suma = n1+n2
+
+print("La suma de los numeros es: ", suma)
+
+
+print ("Hola mundo")
